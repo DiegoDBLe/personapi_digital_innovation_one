d4503e3043fea3f8b44da4f352169b225194ed24
Diego <deigo_batista@hotmail.com>
2021-07-02 01:01:05 -0300

    README

--- personapi/src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -11,3 +11,7 @@ public class PersonapiApplication {
 	}
 
 }
+
+//java: java.lang.IllegalAccessError: class lombok.javac.apt.LombokProcessor
+// (in unnamed module @0x2ab8421c) cannot access class com.sun.tools.javac.processing.JavacProcessingEnvironment (in module jdk.compiler)
+// because module jdk.compiler does not export com.sun.tools.javac.processing to unnamed module @0x2ab8421c
--- personapi/src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -1,17 +1,21 @@
 package one.digitalinnovation.personapi.mapper;
 
+
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
-import org.springframework.web.bind.annotation.Mapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 
-//@Mapper
-//public interface PersonMapper {
-//
-//    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
-//
-//    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
-//    Person toModel(PersonDTO personDTO);
-//
-//    PersonDTO toDTO(Person person);
-//}
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO dto);
+
+    PersonDTO toDTO(Person dto);
+}
